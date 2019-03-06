ab665f2f7960289eca1bb8da5e8b14c5d2744a6e
Myles Doyle <myles.doyle@ichec.ie>
2019-03-06 11:11:07 +0000

    Fixed the issue with the NCNOT and NCUnitary so that they work and follow the method from Nielsen and Chuang. The preious version which makes unjust assumptions by using measurements to manipulate the states still exists and is commented out. This could be fixed so that it is a 'cheat' version which uses less qubits but peeks at the states' amplitudes mid operation. This might be useful if scaling is an issue.

--- Qubit_Extended/include/QubitCircuit.hpp
+++ Qubit_Extended/include/QubitCircuit.hpp
@@ -14,6 +14,8 @@ class QubitCircuit: public QubitRegister<Type>{
         void ApplyMeasurement(std::size_t qubit);
         //void ApplyNCPauliX(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target);             // Won't work ing general since measurement will destroy states in a superposition
         //void ApplyNCUnitary(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target, openqu::TinyMatrix<Type, 2, 2, 32> U);      // Won't work ing general since measurement will destroy states in a superposition
+        void ApplyNCPauliX(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target, int n);             
+        void ApplyNCUnitary(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target, openqu::TinyMatrix<Type, 2, 2, 32> U, int n);      
 };
 
 // Constructor initialising RNG
@@ -37,6 +39,64 @@ void QubitCircuit<Type>::ApplyMeasurement(std::size_t qubit){
 
         // Renormalize state coefficients
         this->Normalize();
+}
+
+// Applies N qubit controlled PauliX where N is the length of the input.
+//      It is assumed that the ancilla registers are in the state |0>.
+//      All ancilla qubits are reset to |0> afterwards.
+//
+//      Requires one less ancilla qubits than control qubits, passed in as n control qubits.
+template <class Type>
+void QubitCircuit<Type>::ApplyNCPauliX(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target, int n){
+    this->ApplyToffoli(input[0],input[1],ancilla[0]);
+
+    // Apply Toffoli repeatedly and dependent on ancillary qubit set by previous iteration of
+    // loop using other qubit as one of the controls so that the history of the input qubits is
+    // recorded in it.
+    // The controlled ancillary qubit is then reset to zero.
+    for(int j = 2; j < n; j++){
+        this->ApplyToffoli(input[j],ancilla[j-2],ancilla[j-1]);
+    }
+    
+
+    // Apply CNOT on target qubit with the last qubit in ancillar register as control.
+    this->ApplyCPauliX(ancilla[n-2],target[0]);
+
+    // Undo operations on ancilla qubits to return them to their original states
+
+    for(int j = n-1; j > 1; j--){
+        this->ApplyToffoli(input[j],ancilla[j-2],ancilla[j-1]);
+    }
+
+    this->ApplyToffoli(input[0],input[1],ancilla[0]);
+}
+
+// Applies N qubit controlled unitary where N is the length of the input.
+//      It is assumed that the ancilla registers are in the state |0>.
+//      All ancilla qubits are reset to |0> afterwards.
+//
+//      Requires one less ancilla qubits than control qubits, passed in as n control qubits.
+template <class Type>
+void QubitCircuit<Type>::ApplyNCUnitary(vector<std::size_t> input, vector<std::size_t> ancilla, vector<std::size_t> target, openqu::TinyMatrix<Type, 2, 2, 32> U , int n){
+    this->ApplyToffoli(input[0],input[1],ancilla[0]);
+
+    // Apply Toffoli repeatedly and dependent on ancillary qubit set by previous iteration of
+    // loop using other qubit as one of the controls so that the history of the input qubits is
+    // recorded in it.
+    // The controlled ancillary qubit is then reset to zero.
+    for(int j = 2; j < n; j++){
+        this->ApplyToffoli(input[j],ancilla[j-2],ancilla[j-1]);
+    }
+    
+    this->ApplyControlled1QubitGate(ancilla[n-2],target[0], U);
+
+    // Undo operations on ancilla qubits to return them to their original states
+
+    for(int j = n-1; j > 1; j--){
+        this->ApplyToffoli(input[j],ancilla[j-2],ancilla[j-1]);
+    }
+
+    this->ApplyToffoli(input[0],input[1],ancilla[0]);
 }
 
 /*
