687224b0e66c157e782bf9422d8c239075a2b464
Lee J. O'Riordan <lee.oriordan@ichec.ie>
2019-03-06 13:43:48 +0000

    n-qubit decomposition code to assist in state encoding

---
+++ include/nqubit_decompose.hpp
@@ -0,0 +1,95 @@
+/**
+ * @file nqubit_decompose.hpp
+ * @author Lee J. O'Riordan (lee.oriordan@ichec.ie)
+ * @brief Functions for applying n-qubit controlled U gates
+ * @version 0.1
+ * @date 2019-03-06
+ */
+
+#ifndef QNLP_NQUBIT_DECOMPOSE
+#define QNLP_NQUBIT_DECOMPOSE
+
+#include "qureg/qureg.hpp"
+#include "util/tinymatrix.hpp"
+#include <unordered_map>
+
+namespace QNLP{
+
+template <class Type>
+class NQubitDecompose{
+    private:
+        // Maintains a table of pre-calculated matrices to avoid repeated work. Absolute value of the key gives the depth of the sqrt ( U^{1/2^n} ), and the sign indicates if it is the adjoint (negative).
+        std::unordered_map<int, openqu::TinyMatrix<Type, 2, 2, 32>> sqrtMatricesU {};
+        std::unordered_map<int, openqu::TinyMatrix<Type, 2, 2, 32>> sqrtMatricesX {};
+
+    public:
+    NQubitDecompose();
+    NQubitDecompose(openqu::TinyMatrix<Type, 2, 2, 32> U);
+
+    ~NQubitDecompose();
+
+    /**
+     * @brief Add the PauliX and the given unitary U to the maps, both indexed with key 0
+     * 
+     * @param U 
+     */
+    //template <class Type>
+    void initialiseMaps(openqu::TinyMatrix<Type, 2, 2, 32> U);
+
+
+
+    /**
+     * @brief Clears the maps of stored sqrt matrices
+     * 
+     */
+    //template <class Type>
+    void clearMaps();
+
+    /**
+     * @brief Decompose n-qubit controlled op into 1 and 2 qubit gates. Requires the control register qubits to be adjacent. 
+     * 
+     * @tparam Type ComplexDP or ComplexSP 
+     * @param qReg Qubit register
+     * @param qControlStart First index of control qubits. 
+     * @param qControlEnd Last index of control qubits. 
+     * @param qTarget Target qubit for the unitary matrix U
+     * @param U Unitary matrix, U
+     * @param depth Depth of recursion.
+     * @param isPauliX To indicate if the decomposition is of a PauliX matrix.
+     */
+    //template <class Type>
+    void applyNQubitControl(QubitRegister<ComplexDP>& qReg, 
+                        unsigned int qControlStart,
+                        unsigned int qControlEnd,
+                        unsigned int qTarget,
+                        openqu::TinyMatrix<Type, 2, 2, 32> U, 
+                        unsigned int depth, bool isPauliX);
+
+    /**
+     * @brief Calculates the unitary matrix square root (U == VV, where V is returned)
+     * 
+     * @tparam Type ComplexDP or ComplexSP
+     * @param U Unitary matrix to be rooted
+     * @return openqu::TinyMatrix<Type, 2, 2, 32> V such that VV == U
+     */
+    //template <class Type>
+    openqu::TinyMatrix<Type, 2, 2, 32> matrixSqrt(const openqu::TinyMatrix<Type, 2, 2, 32>& U);
+
+    /**
+     * @brief Function to calculate the adjoint of an input matrix
+     * 
+     * @tparam Type ComplexDP or ComplexSP
+     * @param U Unitary matrix to be adjointed
+     * @return openqu::TinyMatrix<Type, 2, 2, 32> U^{\dagger}
+     */
+    //template <class Type>
+    openqu::TinyMatrix<Type, 2, 2, 32> adjointMatrix(const openqu::TinyMatrix<Type, 2, 2, 32>& U);
+
+};
+
+//Instantiate the allowed class types
+template class NQubitDecompose<ComplexSP>;
+template class NQubitDecompose<ComplexDP>;
+
+};
+#endif
