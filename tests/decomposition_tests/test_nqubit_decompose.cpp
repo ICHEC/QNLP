d183f580ba6891d6f331d45d77c712a23d2c201d
Lee J. O'Riordan <lee.oriordan@ichec.ie>
2019-03-06 15:35:55 +0000

    Added test routines for gate decomposition. Shows errors for > 2 controls.

---
+++ tests/decomposition_tests/test_nqubit_decompose.cpp
@@ -0,0 +1,84 @@
+#include "nqubit_decompose.hpp"
+
+#include "qureg/qureg.hpp"
+#include "util/tinymatrix.hpp"
+#include<bitset>
+
+using namespace QNLP;
+
+typedef ComplexDP Type;
+
+void test_decompose(std::size_t num_qubits, openqu::TinyMatrix<Type, 2, 2, 32> U, bool isX){
+    unsigned int c_start = 0, c_end = num_qubits-2;
+    unsigned int target = num_qubits-1;
+
+    QubitRegister<ComplexDP> psi1(num_qubits, "base", 0);
+    unsigned int shifts;
+    double p = -1.;
+    for( unsigned int cc = 0; cc < 1<<(num_qubits-1); cc++ ){
+        shifts = 0;
+        psi1.Initialize("base",0);
+            
+        for( unsigned int q = 1; q <= cc; q = q << 1){
+            if(cc & q > 0){
+                psi1.ApplyPauliX(shifts);
+            }
+            shifts++;
+        } 
+        NQubitDecompose<ComplexDP> nqd(U);
+        nqd.applyNQubitControl(psi1, c_start, c_end, target, U, 0, isX);
+        std::cout << "Test " << cc << " Pattern=" << std::bitset<8>(cc);
+        std::cout << " P(1)=" << psi1.GetProbability( target ) << std::endl;
+    }
+}
+
+int main(int argc, char **argv){
+
+    openqu::mpi::Environment env(argc, argv);
+    if (env.is_usefull_rank() == false) return 0;
+
+    int myid = env.rank();
+
+    {
+        if (myid == 0) {
+            printf("\n --- n-Qubit Controlled U Test --- \n");
+            std::cout << "The 3-qubit register (gates 0 to 2) is initialized in state |000>. \n"
+                << "The controlled U test uses U=X, and as such implements a Toffoli decomposition. \n"
+                << "The resulting logic table should operate as follows \n"
+                << "C C -> T\n"
+                << "0 0 -> 0\n"
+                << "0 1 -> 0\n"
+                << "1 0 -> 0\n"
+                << "1 1 -> 1\n";
+        }
+        openqu::TinyMatrix<Type, 2, 2, 32> X;
+        X(0,0) = {0.,  0.};
+        X(0,1) = {1., 0.};
+        X(1,0) = {1., 0.};
+        X(1,1) = {0.,  0.};
+
+        if (myid == 0){
+            //Start with simple CCNot test case; 2 control lines, 1 target
+            std::cout << "################################################" << std::endl;
+            std::cout << "Testing 2 control lines" << std::endl;
+            test_decompose(3, X, true);
+            std::cout << "################################################" << std::endl;
+
+            std::cout << "################################################" << std::endl;
+            std::cout << "Testing 3 control lines" << std::endl;
+            test_decompose(4, X, true);
+            std::cout << "################################################" << std::endl;
+
+            std::cout << "################################################" << std::endl;
+            std::cout << "Testing 4 control lines" << std::endl;
+            test_decompose(5, X, true);
+            std::cout << "################################################" << std::endl;
+
+            std::cout << "################################################" << std::endl;
+            std::cout << "Testing 5 control lines" << std::endl;
+            test_decompose(6, X, true);
+            std::cout << "################################################" << std::endl;
+        }
+    }
+
+}
