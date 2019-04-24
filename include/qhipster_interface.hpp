085a6aa365c128cf8e3acbdd0fcf59516b2edc0e
Lee <lee.oriordan@ichec.ie>
2019-04-24 10:21:47 +0100

    Merged interface branch

--- include/qhipster_interface.hpp
+++ include/qhipster_interface.hpp
@@ -28,6 +28,10 @@ class IntelSimulator : SimulatorInterface< QbitRegister<ComplexDP>, QbitRegister
     IntelSimulator(int numQubits) : numQubits(numQubits), qubitRegister(numQubits, "base", 0){
     }
 
+    ~IntelSimulator(){
+        //TODO: ensure everything is safely freed here
+    }
+
     void applyGateX(std::size_t qubitIndex){
         qubitRegister.applyPauliX(qubitIndex);
     }
@@ -43,6 +47,88 @@ class IntelSimulator : SimulatorInterface< QbitRegister<ComplexDP>, QbitRegister
     void applyGateH(std::size_t qubitIndex){
         qubitRegister.applyPauliY(qubitIndex);
     }
+
+    // Defining gate operations
+    void applyGate(std::array<ComplexDP>& gate){
+        TODO
+    }
+
+    // Defining gate operations
+    void applyGate(std::string gateName){
+        TODO
+    }
+
+    const TinyMatrix& getGate(std::string gateName) const {
+        TODO 
+        //May be difficult to generalise
+    }
+
+    void applyGateSqrtX(std::size_t qubitIndex){
+        qubitRegister.applyPauliSqrtX(std::size_t qubitIndex);
+    };
+
+    //Arbitrary rotation along axis; given in radians
+    void applyGateRotX(std::size_t qubitIndex) {
+        qubitRegister.applyRotationX(qubitIndex);
+    };
+    void applyGateRotY(std::size_t qubitIndex) {
+        qubitRegister.applyRotationY(qubitIndex);
+    };
+    void applyGateRotZ(std::size_t qubitIndex) {
+        qubitRegister.applyRotationZ(qubitIndex);
+    };
+
+    //2 qubit gates
+    void applyGateCU(std::size_t control, std::size_t target, TinyMatrix<ComplexDP, 2, 2, 32> & U){
+        qubitRegister.applyControlled1QubitGate(control, target, U);
+    }
+
+    void applyGateCX(const std::size_t control, const std::size_t target){
+        qubitRegister.applyCPauliX(control, target);
+    }
+    void applyGateCY(const std::size_t control, const std::size_t target){
+        qubitRegister.applyCPauliY(control, target);
+    }
+    void applyGateCZ(const std::size_t control, const std::size_t target){
+        qubitRegister.applyCPauliZ(control, target);
+    }
+    void applyGateCH(const std::size_t control, const std::size_t target){
+        qubitRegister.applyCHadamard(control, target);
+    }
+
+    void applyGateCRotX(const std::size_t control, const std::size_t target, const double theta){
+        qubitRegister.applyCRotationX(control, target, theta);
+    }
+    void applyGateCRotY(const std::size_t control, const std::size_t target, const double theta){
+        qubitRegister.applyCRotationY(control, target, theta);
+    }
+    void applyGateCRotZ(const std::size_t control, const std::size_t target, const double theta){
+        qubitRegister.applyCRotationZ(control, target, theta);
+    }
+
+    void applyGateSwap(std::size_t q1, std::size_t q2){
+        qubitRegister.applySwap(q1, q2);
+    }
+    void applyGateSqrtSwap(){
+        qubitRegister.applySqrtISwap
+    }
+    void applyGatePhaseShift(){
+        //qubitRegister.apply
+    }
+
+    //3 qubit gates
+    virtual void applyGateToffoli();
+    virtual void applyGateFredkin();
+
+    //Defining Qubit operations
+    QubitRegisterType getQubitRegister() { return this->qubitRegister; }
+    const QubitRegisterType& getQubitRegister() const { return this->qubitRegister; };
+
+    virtual QubitType getQubit();
+    virtual const QubitType& getQubit() const;
+
+    std::size_t getNumQubits() { return numQubits; }
+
 };
 
 #endif
---
+++ src/qhipster_simulator.cpp
@@ -0,0 +1,4 @@
+#include "../include/sim_interface.hpp"
+#include "../include/qhipster_interface.hpp"
+
+using namespace QNLP;
