d4d1cc2f68bb409d09a995bbc81b2e7bb646b346
Lee <lee.oriordan@ichec.ie>
2019-05-21 11:12:35 +0100

    Updated method documentation

--- modules/simulator/Simulator.hpp
+++ modules/simulator/Simulator.hpp
@@ -180,28 +180,78 @@ namespace QNLP{
         //                           Single qubit gates
         //##############################################################################
 
+        /**
+         * @brief Apply the Pauli X gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
         void applyGateX(std::size_t qubit_idx){ 
             static_cast<DerivedType&>(*this).applyGateX(qubit_idx); 
         };
+        /**
+         * @brief Apply the Pauli Y gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
         void applyGateY(std::size_t qubit_idx){
             static_cast<DerivedType&>(*this).applyGateY(qubit_idx);
         }
+        /**
+         * @brief Apply the Pauli Z gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
         void applyGateZ(std::size_t qubit_idx){
             static_cast<DerivedType&>(*this).applyGateZ(qubit_idx);
         }
+        /**
+         * @brief Apply the Identity gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
         void applyGateI(std::size_t qubit_idx){
             static_cast<DerivedType&>(*this).applyGateI(qubit_idx); 
         }
+        /**
+         * @brief Apply the Hadamard gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
         void applyGateH(std::size_t qubit_idx){
             static_cast<DerivedType&>(*this).applyGateH(qubit_idx); 
         }
-        void applyGateSqrtX(std::size_t qubit_idx){}
-        void applyGateRotX(std::size_t qubit_idx, double angle_rad){
+        /**
+         * @brief Apply the Sqrt{Pauli X} gate to the given qubit
+         * 
+         * @param qubit_idx 
+         */
+        void applyGateSqrtX(std::size_t qubit_idx){
+            static_cast<DerivedType&>(*this).applyGateSqrtX(qubit_idx); 
+        }
+        /**
+         * @brief Apply the given Rotation about X-axis to the given qubit
+         * 
+         * @param qubit_idx Index of qubit to rotate about X-axis
+         * @param angle_rad Rotation angle
+         */
+        void applyGateRotX(std::size_t qubit_idx, double v){
             static_cast<DerivedType&>(*this).applyGateRotX(qubit_idx, angle_rad);
         }
+        /**
+         * @brief Apply the given Rotation about Y-axis to the given qubit
+         * 
+         * @param qubit_idx Index of qubit to rotate about Y-axis
+         * @param angle_rad Rotation angle
+         */
         void applyGateRotY(std::size_t qubit_idx, double angle_rad){
             static_cast<DerivedType&>(*this).applyGateRotY(qubit_idx, angle_rad);
         }
+        /**
+         * @brief Apply the given Rotation about Z-axis to the given qubit
+         * 
+         * @param qubit_idx Index of qubit to rotate about Z-axis
+         * @param angle_rad Rotation angle
+         */
         void applyGateRotZ(std::size_t qubit_idx, double angle_rad){
             static_cast<DerivedType&>(*this).applyGateRotZ(qubit_idx, angle_rad);
         }
@@ -209,18 +259,19 @@ namespace QNLP{
         /**
          * @brief Apply arbitrary user-defined unitary gate to qubit at qubit_idx
          * 
-         * @param U User-defined unitary 2x2 matrix of templated type GateType
+         * @param U User-defined unitary 2x2 matrix of templated type Mat2x2Type
          * @param qubit_idx Index of qubit to apply gate upon
          */
-        //virtual void applyGateU(const decltype(std::declval<DerivedType&>().getGateX()) &U, std::size_t qubit_idx) = 0;
+        template<class Mat2x2Type>
+        void applyGateU(const Mat2x2Type &U, std::size_t qubit_idx){
+            static_cast<DerivedType*>(this)->applyGateU(U, qubit_idx);
+        }
 
         /**
          * @brief Get the Pauli-X gate; returns templated type GateType
-
-         * 
          * @return GateType Templated return type of Pauli-X gate
          */
-        decltype(auto) getGateX() {//-> decltype( dynamic_cast<DerivedType>(*this).getGateX() ){
+        decltype(auto) getGateX() {
             return static_cast<DerivedType&>(*this).getGateX();
         }
 
@@ -228,14 +279,14 @@ namespace QNLP{
          * @brief Get the Pauli-Y gate; must be overloaded with appropriate return type
          * @return GateType Templated return type of Pauli-Y gate
          */
-        decltype(auto) getGateY(){ //-> decltype(std::declval<DerivedType&>().getGateY());
+        decltype(auto) getGateY(){
             return static_cast<DerivedType&>(*this).getGateY();
         }
         /**
          * @brief Get the Pauli-Z gate; must be overloaded with appropriate return type
          * @return GateType Templated return type of Pauli-Z gate
          */
-        decltype(auto) getGateZ(){ //-> decltype(std::declval<DerivedType&>().getGateZ());
+        decltype(auto) getGateZ(){
             return static_cast<DerivedType&>(*this).getGateZ();
         }
         /**
@@ -249,9 +300,10 @@ namespace QNLP{
          * @brief Get the Hadamard gate; must be overloaded with appropriate return type
          * @return GateType Templated return type of Hadamard gate
          */
-        decltype(auto) getGateH(){// -> decltype(std::declval<DerivedType&>().getGateH());
+        decltype(auto) getGateH(){
             return static_cast<DerivedType*>(this)->getGateH();
         }
+
         //##############################################################################
         //                                  2 qubit gates
         //##############################################################################
@@ -278,23 +330,55 @@ namespace QNLP{
             return static_cast<DerivedType*>(this)->applyGateSwap(qubit_idx0,qubit_idx1);
         }
 
+        /**
+         * @brief Performs Sqrt SWAP gate between two given qubits (half way SWAP)
+         * 
+         * @param qubit_idx0 Qubit index 0
+         * @param qubit_idx1 Qubit index 1
+         */
         void applyGateSqrtSwap(std::size_t qubit_idx0, std::size_t qubit_idx1){
             return static_cast<DerivedType*>(this)->applyGateSqrtSwap(qubit_idx0,qubit_idx1);
         }
         
+        /**
+         * @brief Apply phase shift to given Qubit; [[1 0] [0 exp(i*angle)]]
+         * 
+         * @param angle Angle of phase shift in rads
+         * @param qubit_idx Qubit index to perform phase shift upon
+         */
         void applyGatePhaseShift(double angle, std::size_t qubit_idx){
             static_cast<DerivedType*>(this)->applyGatePhaseShift(angle, qubit_idx);
         }
 
+        /**
+         * @brief Perform controlled phase shift gate 
+         * 
+         * @param angle Angle of phase shift in rads
+         * @param control Index of control qubit
+         * @param target Index of target qubit
+         */
         void applyGateCPhaseShift(double angle, std::size_t control, std::size_t target){
             static_cast<DerivedType*>(this)->applyGateCPhaseShift(angle, control, target);
         }
 
-        //3 qubit gates
+        /**
+         * @brief Controlled controlled NOT (CCNOT, CCX) gate
+         * 
+         * @param ctrl_qubit0 Control qubit 0
+         * @param ctrl_qubit1 Control qubit 1
+         * @param target_qubit Target qubit
+         */
         void applyGateCCX(std::size_t ctrl_qubit0, std::size_t ctrl_qubit1, std::size_t target_qubit){
             static_cast<DerivedType*>(this)->applyGateCCX(ctrl_qubit0, ctrl_qubit1, target_qubit);
         }
 
+        /**
+         * @brief Controlled SWAP gate
+         * 
+         * @param ctrl_qubit Control qubit
+         * @param qubit_swap0 Swap qubit 0
+         * @param qubit_swap1 Swap qubit 1
+         */
         void applyGateCSwap(std::size_t ctrl_qubit, std::size_t qubit_swap0, std::size_t qubit_swap1){
             assert( static_cast<DerivedType*>(this)->getNumQubits() > 2 );
             //The requested qubit indices must be available to use within the register range
@@ -305,24 +389,53 @@ namespace QNLP{
             static_cast<DerivedType*>(this)->applyGateCSwap(ctrl_qubit, qubit_swap0, qubit_swap1);
         }
 
-        //Defining Qubit operations
-        //virtual decltype(auto)& getQubitRegister() -> decltype(DerivedType::getQubitRegister()) & = 0;
-        decltype(auto) getQubitRegister(){ //-> decltype(std::declval<DerivedType&>().getQubitRegister())=0;
+        /**
+         * @brief Get the underlying qubit register object
+         * 
+         * @return decltype(auto) the underlying bound qubit register
+         */
+        decltype(auto) getQubitRegister(){ 
            return static_cast<DerivedType*>(this)->getQubitRegister();
         }
 
+        /**
+         * @brief Get the number of Qubits
+         * 
+         * @return std::size_t Number of qubits in register
+         */
         std::size_t getNumQubits(){
             return static_cast<DerivedType*>(this)->getNumQubits();    
         }
 
+        /**
+         * @brief Apply the forward Quantum Fourier transform (QFT) to the given register index range
+         * 
+         * @param minIdx Lowest qubit index of the QFT range
+         * @param maxIdx Highest qubit index of the QFT range
+         */
         void applyQFT(std::size_t minIdx, std::size_t maxIdx){
             QFT<decltype(static_cast<DerivedType&>(*this))>::applyQFT(static_cast<DerivedType&>(*this), minIdx, maxIdx);
         }
 
+        /**
+         * @brief Apply the inverse Quantum Fourier transform (IQFT) to the given register index range
+         * 
+         * @param minIdx Lowest qubit index of the IQFT range
+         * @param maxIdx Highest qubit index of the IQFT range
+         */
         void applyIQFT(std::size_t minIdx, std::size_t maxIdx){
             QFT<decltype(static_cast<DerivedType&>(*this))>::applyIQFT(static_cast<DerivedType&>(*this), minIdx, maxIdx);
         }
 
+        /**
+         * @brief Apply n-control unitary gate to the given qubit target
+         * 
+         * @tparam Mat2x2Type 2x2 Matrix type of unitary gate in the format expected by the derived simulator object
+         * @param U 2x2 unitary matrix
+         * @param minIdx Lowest index of the control lines expected for nCU
+         * @param maxIdx Highest index of the control lines expected for the nCU
+         * @param target Target qubit index to apply nCU
+         */
         template<class Mat2x2Type>
         void applyGateNCU(const Mat2x2Type& U, std::size_t minIdx, std::size_t maxIdx, std::size_t target){
             NCU<DerivedType> n;
@@ -330,6 +443,10 @@ namespace QNLP{
             n.applyNQubitControl(static_cast<DerivedType&>(*this), minIdx, maxIdx, target, U, 0, false);
         }
 
+        /**
+         * @brief (Re)Initialise the underlying register of the encapsulated simulator to well-defined state (|0....0>)
+         * 
+         */
         void initRegister(){
             static_cast<DerivedType*>(this)->initRegister(); 
         }
