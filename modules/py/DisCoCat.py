cc44f3f58c00328f77b452ee5701f44c688cfe9c
Lee <lee.oriordan@ichec.ie>
2019-05-29 13:40:31 +0100

    DB support for DisCoCat datastructures

--- modules/py/DisCoCat.py
+++ modules/py/DisCoCat.py
@@ -11,7 +11,43 @@ from qnlp_db import qnlp_db
 
 #Use mixin to modify the insert statements and modify the structure of database
 class qdb_mixin(qnlp_db):
-    def db_insert_custom(self, values: Dict, data_type="noun", table_name="qnlp"):
+
+    def create_table_discocat(self, table_name="qnlp"):
+        """
+        Create the database table for tagging the required data. The DB has columns
+        for dataset ('basis' or 'corpus'), data_type ('verb', 'noun', etc.), token (the string value), mapping_idx (the index of the mapped binary value; for superpos states, this index labels the number of values in the superposition), map_bin_id (the binary value representing the quantum state in the register), map_coeff_r/i (real and imaginary coefficients of the map_bin_id state), mapping_dir (indicates the direction of the mapping; may not be used).
+        """
+        cr_tbl = """CREATE TABLE {}(
+                    id INTEGER PRIMARY KEY, 
+                    dataset TEXT,
+                    data_type TEXT,
+                    token TEXT,
+                    mapping_idx INTEGER,
+                    map_bin_id INTEGER,
+                    map_coeff_r REAL,
+                    map_coeff_i REAL,
+                    mapping_dir INTEGER
+                    );""".format(table_name)
+        conn = super(qdb_mixin, self).connect_db()
+        c = conn.cursor()
+
+        try:
+            c.execute(cr_tbl)
+
+        except sqlite3.OperationalError as oe:
+            remove_db = input("Table '{}' already exists. Remove? y/n: ".format(table_name))
+            if remove_db is "y":
+                self.drop_table(table_name)
+                self.create_table_discocat(table_name)
+
+        except Exception as e:
+            print("SQLITE exception thrown: {0}".format(e), "Exiting program.")
+            exit()
+
+        finally:
+            conn.commit()
+
+    def db_insert_discocat(self, values, dataset="basis", data_type="noun", table_name="qnlp"):
         """
         Insert the tag to binary encoding mapping into the DB.
 
@@ -20,7 +56,6 @@ class qdb_mixin(qnlp_db):
         table_name  -- Name of table to store in DB
         """
 
-        #Proposed modification; weight set to 0 currently
         '''
         The DB insert operation below assumes the value field of a key in DB is a tuple,
         containing (binary_id, weight of occurrence), where weight of occurrence cant be
@@ -31,15 +66,29 @@ class qdb_mixin(qnlp_db):
         '''
         conn = super(qdb_mixin, self).connect_db()
         c = conn.cursor()
-        super(qdb_mixin, self).create_table(table_name)
+        self.create_table_discocat(table_name)
         
-        for k,v in values.items():
-            if not str(k).isnumeric():
-                c.execute('''INSERT INTO {} 
-                    (type, name, bin_id, weight ) VALUES(?,?,?,?)'''.format(table_name), 
-                    (data_type, k, int(v,2), 0 )
+        for corpus_token, superpos in values.items():
+            l_superpos = len(superpos)
+            for idx, (distance_measure, basis_state) in enumerate(superpos):
+                c.execute("""INSERT INTO {} ( 
+                    dataset, 
+                    data_type, 
+                    token, 
+                    mapping_idx, 
+                    map_bin_id, 
+                    map_coeff_r, 
+                    map_coeff_i, 
+                    mapping_dir ) VALUES(?,?,?,?,?,?,?,?)""".format(table_name), 
+                    (dataset, 
+                    data_type, 
+                    corpus_token, 
+                    idx, 
+                    basis_state, 
+                    distance_measure.real, 
+                    distance_measure.imag, 
+                    0)
                 )
-                print (data_type, k, int(v,2), 0 )
         conn.commit()
 
 ###############################################################################
@@ -113,13 +162,14 @@ class DisCoCat:
             distance_func = lambda x : [1.0/i for i in x]
 
         d_map = {}
-
+        
         # Find location of basis words in token list. Store in dict as d_map
         for b in basis:
             indices = list(filter(lambda x: corpus_list[x] == b[0], range(len(corpus_list))))
             d_map.update({b[0] : np.array(indices)})
 
         word_map = {}
+
         # map distance between basis words and other words in token list
         for idx, word in enumerate(corpus_list):
             for b_k, b_v in d_map.items():
@@ -192,25 +242,19 @@ class DisCoCat:
             for b_key, b_val in bit_map[1].items():
                 f.write(b_key + " $\\rightarrow " + tex_string_format_bit.format(b_val) + "$\\\\ ")
             f.write("\\end{multicols}")
-
             f.write("\\noindent\\rule{\\textwidth}{1pt} \n")
             f.write("\\noindent\\rule{\\textwidth}{1pt} \n")
-
             f.write("\\section{Encoding} \n")
-
             for token, basis_map in mapping.items():
                 f.write(r"\begin{align}\vert \textrm{" + token + "} \\rangle &= \\\\ \n &" )
                 for i,b in enumerate(basis_map):
                     if( i != 0 ):
                         if(i%3 == 0):
                             f.write(r" \\ & ")
-
                     f.write("{0:.3f}".format(round(b[0],3)))
                     f.write(tex_string_format_bit.format(b[1]) )
-
                     if(i != len(basis_map) - 1):
                         f.write(r"+")
-
                     f.write(" \\nonumber ")
                 f.write(r"""\end{align}""")
                 f.write("\\noindent\\rule{\\textwidth}{1pt} \n")
@@ -222,13 +266,14 @@ if __name__ == "__main__":
     CorpusFile = os.sys.argv[1]
 
     dcc = DisCoCat()
+    db = qdb_mixin(os.path.basename(CorpusFile),'.')
 
     # Load the corpus
     corpus_text = dcc.load_corpus(CorpusFile)
     tokens = dcc.tokenise_corpus(corpus_text)
 
-    basis_v = dcc.define_basis_words(tokens['verbs'], 50)
-    basis_n = dcc.define_basis_words(tokens['nouns'], 50)
+    basis_v = dcc.define_basis_words(tokens['verbs'], 10)
+    basis_n = dcc.define_basis_words(tokens['nouns'], 10)
 
     bit_map_v = dcc.map_to_bitstring(basis_v)
     bit_map_n = dcc.map_to_bitstring(basis_n)
@@ -246,5 +291,8 @@ if __name__ == "__main__":
 
     dcc.latex_states(bit_map_n, noun_map, "nouns")
     dcc.latex_states(bit_map_v, verb_map, "verbs")
+
+    db.db_insert_discocat(n_states, "corpus", "noun", "corpus")
+    db.db_insert_discocat(v_states, "corpus", "verb", "corpus")
 
 ###############################################################################
