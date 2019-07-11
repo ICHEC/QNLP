c353d89317503f8a4e3eb61d514f67635af306b9
Lee <lee.oriordan@ichec.ie>
2019-07-11 11:22:59 +0100

    Working circuit diagram splitter. Boxes each cct up to given depth, and puts into tabular env

--- modules/py/cct.py
+++ modules/py/cct.py
@@ -35,7 +35,7 @@ def load_data_csv(csv_file):
             cct.append(ctrl_line_column(row[0], int(row[1]), int(row[2]), 5) )
     return cct
 
-def latex_cct(data_file, file_name="cct"):
+def latex_cct(data_file, file_name="cct", max_depth=16):
     """
     LaTeX file outputter for quantum circuit generation.
     """
@@ -45,19 +45,45 @@ def latex_cct(data_file, file_name="cct"):
     depth = ncu_cct_depth(num_qubits-1)
 
     with open(file_name + ".tex", "w") as f:
-        f.write("\\documentclass[crop]{standalone} \n \\usepackage{amsmath} \\usepackage{tikz} \\usetikzlibrary{quantikz} \n \\begin{document} \\begin{quantikz}\n")
-        #Transposes the data so that q rows of length n exist, rather than n cols of length q
-        cct_list_qubit= list(map(list, zip(*cct_array)))
-        for q in cct_list_qubit:
-            f.write("".join(q) + "\\\\ \n")
-        f.write(r"""\end{quantikz}""")
+        f.write("\\documentclass{standalone} \n \\usepackage{amsmath} \\usepackage{tikz} \\usetikzlibrary{quantikz} \n \\begin{document}\n")
+        # Split the circuit on a given depth boundary
+
+        # Due to issues with latex variables ending with numeric indices, appending letters to the temporary savebox commands allows us to generate multiple of these.
+        # As depth is an issue with circuits, we currently expect the output not to exceed n-choose-k of 52-C-4 = 270725 combinations
+        # This will suffice until later.
+        import string, itertools
+        s_list = [i for i in string.ascii_letters]
+        box_str = r"\Box"
+        label_iterator = itertools.combinations(s_list,4)
+        box_labels = []
+        for i in range(0, depth, max_depth):
+            local_label = box_str + "".join( next(label_iterator))
+            box_labels.append(local_label)
+            f.write(r"\newsavebox{{{}}}".format(local_label))
+            f.write(r"\savebox{{{}}}{{".format(local_label))
+            f.write("\\begin{quantikz}[row sep={0.7cm,between origins}]")
+            #Transposes the data so that q rows of length n exist, rather than n cols of length q
+            if(i + max_depth < depth):
+                cct_list_qubit= list(map(list, zip(*cct_array[i:i+max_depth])))
+            else:
+                cct_list_qubit= list(map(list, zip(*cct_array[i:])))
+            for q in cct_list_qubit:
+                f.write("\\qw &".join(q) + "\\qw \\\\")
+            f.write(" \\end{quantikz} }\n")
+            f.write("\n")
+        f.write("\\begin{tabular}{c}\n")
+        for l in box_labels:
+            f.write(r"\usebox{{{}}} \\".format(l))
+            f.write("\n \\vspace{2em} \\\\")
+
+        f.write("\\end{tabular}\n")
         f.write(r"\end{document}")
 
 if __name__== "__main__":
     import os
-    args = os.sys.argv[]
-    if len(args < 1):
-        print("Please specify the file to load")
-        return
+    args = os.sys.argv
+    if len(args) < 2:
+        print("Please specify the file to load, and output filename to save: python cct.py <CSV> <>")
+        exit()
     
-    latex_cct(args[1])
+    latex_cct(args[1], args[2])
