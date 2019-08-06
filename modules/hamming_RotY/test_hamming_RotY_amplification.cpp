535622224b18198f6c1b6451a7b35ea1cc719e9a
Myles Doyle <myles.doyle@ichec.ie>
2019-08-06 17:34:14 +0100

    Getting unit tests for HammingRotY to work

--- modules/hamming_RotY/test_hamming_RotY_amplification.cpp
+++ modules/hamming_RotY/test_hamming_RotY_amplification.cpp
@@ -13,42 +13,62 @@ using namespace Catch::Matchers;
 //For simplicity, enabling complex double only
 typedef ComplexDP Type;
 
-template class HammingDistance<IntelSimulator>;
+template class HammingDistanceRotY<IntelSimulator>;
 
 
-TEST_CASE("Test Hamming distance routine","[Hamming]"){
-    SECTION("Testing distance calc"){
+TEST_CASE("Test Hamming distance with Roatation about y axis routine","[HammingRotY]"){
+    const std::size_t max_qubits = 5;
+    double mach_eps = 7./3. - 4./3. -1.;
+
+    std::size_t len_reg_memory;
+    std::size_t len_reg_ancilla;
+    std::size_t num_bin_pattern;
+
+    std::size_t test_pattern = 3;
+
+    for(std::size_t num_qubits = 2; num_qubits < max_qubits; num_qubits++){
+        DYNAMIC_SECTION("Testing " << num_qubits << " qubits"){
+
+            IntelSimulator sim(num_qubits);
+
+            len_reg_memory = (num_qubits - 2) / 2;
+            len_reg_ancilla = len_reg_memory + 2;
+            num_bin_pattern = pow(2,len_reg_memory) - 2;
+
+            // Set up registers to store indices
+            std::vector<std::size_t> reg_memory(len_reg_memory);
+            for(std::size_t i = 0; i < len_reg_memory; i++){
+                reg_memory[i] = i;
+            }
+            std::vector<std::size_t> reg_ancilla(len_reg_ancilla);
+            for(std::size_t i = 0; i < len_reg_ancilla; i++){
+                reg_ancilla[i] = i + len_reg_memory;
+            }
+
+            // Init data to encode
+            std::vector<std::size_t> vec_to_encode(num_bin_pattern);
+            for(std::size_t i = 2; i < num_bin_pattern; i++){
+                vec_to_encode[i-2] = i;
+            }
+
+
+            // Encode
+            sim.encodeBinToSuperpos_unique(reg_memory, reg_ancilla, vec_to_encode, len_reg_memory);
+
+            // Compute Hamming distance
+            sim.applyHammingDistanceRotY(test_pattern, reg_memory, reg_ancilla, len_reg_memory, num_bin_pattern);
+
+            sim.PrintStates("After encoding: ");  
+
+
+            REQUIRE(1 == 1);
+
+        }
+    }
+}
+
 /*
-        std::size_t num_qubits = 10;
-
-        std::size_t test_pattern = 2;
-
-        std::size_t len_reg_memory = (num_qubits - 2) / 2;
-        std::size_t len_reg_ancilla = len_reg_memory + 2;
-        std::size_t num_bin_pattern = pow(2,len_reg_memory) - 2;
-
-        IntelSimulator sim(num_qubits);
-        REQUIRE(sim.getNumQubits() == num_qubits);
-
-        // Set up registers to store indices
-        std::vector<std::size_t> reg_memory(len_reg_memory);
-        for(std::size_t i = 0; i < len_reg_memory; i++){
-            reg_memory[i] = i;
-        }
-        std::vector<std::size_t> reg_ancilla(len_reg_ancilla);
-        for(std::size_t i = 0; i < len_reg_ancilla; i++){
-            reg_ancilla[i] = i + len_reg_memory;
-        }
-
-        // Init data to encode
-        std::vector<std::size_t> vec_to_encode(num_bin_pattern);
-        for(std::size_t i = 2; i < num_bin_pattern; i++){
-            vec_to_encode[i-2] = i;
-        }
-
         std::size_t val;
-        // Encode
-        sim.encodeBinToSuperpos_unique(reg_memory, reg_ancilla, vec_to_encode, len_reg_memory);
 
         // Compute Hamming distance
         HammingDistance<DerivedType> hamming_operator(len_reg_memory);
@@ -59,8 +79,6 @@ TEST_CASE("Test Hamming distance routine","[Hamming]"){
 
         CHECK_THAT(vec_to_encode, VectorContains(val));
         */
-    }
-}
 
 /*
 int main( int argc, char* argv[] ) {
