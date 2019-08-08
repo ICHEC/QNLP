a740fdfd94cde3af08ab0c94ea4d1ddeeed64910
Myles Doyle <myles.doyle@ichec.ie>
2019-08-08 17:15:16 +0100

    Adding unit test for Hamming to check amplitudes are set appropriately. This is a WIP but is almost complete.

--- modules/hamming_RotY/test_hamming_RotY_amplification.cpp
+++ modules/hamming_RotY/test_hamming_RotY_amplification.cpp
@@ -7,6 +7,8 @@
 
 #include <bitset>
 
+#define IS_SET(byte,bit) (((byte) & (1UL << (bit))) >> (bit))
+
 using namespace QNLP;
 using namespace Catch::Matchers;
 
@@ -15,7 +17,48 @@ typedef ComplexDP Type;
 
 template class HammingDistanceRotY<IntelSimulator>;
 
-//using namespace Catch::Detail;
+std::size_t calc_hammongDist(std::size_t pattern1, std::size_t pattern2, std::size_t len_bin_pattern){
+    std::size_t num_diffs = 0;
+
+    for(int i = 0; i < len_bin_pattern; i++){
+        num_diffs += (IS_SET(pattern1,i) == IS_SET(pattern2,i));
+    }
+    return num_diffs;
+}
+
+std::map<std::size_t, double> expected_amplitude(std::vector<std::size_t>& target_vec, std::size_t test, std::size_t len_bin_pattern){
+    const std::size_t num_bin_pattern = target_vec.size();
+    const double sin_rotation_theta = sin(0.5 * M_PI / len_bin_pattern);
+  //  std::size_t total_similar_bits = 0;
+
+    // Calculate Hamming Distance for each training pattern to test pattern
+    std::map<std::size_t, double> exp_amp; 
+    for(std::size_t i = 0; i < num_bin_pattern; i++){
+        exp_amp.insert(pair<std::size_t, double>(target_vec[i],(double) calc_hammongDist(target_vec[i],test, len_bin_pattern)));
+    }
+
+
+    for(auto& [k,v] : exp_amp){
+        v = pow(sin_rotation_theta, v)*(v > 0.0);
+//        v *= v;
+    }   
+
+    // Calculate total number of bits that were in common with test pattern between all training patterns
+    for(auto& [k,v] : exp_amp){
+   //     total_similar_bits += (std::size_t) v;
+    }
+    for(auto& [k,v] : exp_amp){
+//        v /= (double) total_similar_bits;
+    }
+    for(auto& [k,v] : exp_amp){
+ //       v = sin(v);
+    }
+
+
+
+
+    return exp_amp;
+}
 
 TEST_CASE("Test Hamming distance with Roatation about y axis routine","[hammingroty]"){
     const std::size_t max_qubits = 3;
@@ -64,31 +107,41 @@ TEST_CASE("Test Hamming distance with Roatation about y axis routine","[hammingr
             sim.applyHammingDistanceRotY(test_pattern, reg_memory, reg_ancilla, len_reg_memory, num_bin_pattern);
 
 
-//            sim.PrintStates("After Hamming: ");  
+            sim.collapseToBasisZ(reg_ancilla[len_reg_ancilla-2], 1);
+
+            sim.PrintStates("After Hamming: ");  
+
+
+            std::map<std::size_t, double> exp_amp =  expected_amplitude(vec_to_encode, test_pattern, len_reg_memory);
 
             for(int i = 0; i < num_bin_pattern; i++){
-                //cout << i << "\t" << r[i] << endl;
+ //               cout << i << "\t" << r[i] << endl;
+                REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+
             }
-//cout << "------------" << endl;
+cout << "------------" << endl;
             for(int i = num_bin_pattern; i < pow(2,2*len_reg_memory); i++){
-                //cout << i << "\t" << r[i] << endl;
+//                cout << i << "\t" << r[i] << endl;
                 REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
                 REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
             }
- //cout << "------------" << endl;
+ cout << "------------" << endl;
             for(int i = pow(2,2*len_reg_memory); i < pow(2,2*len_reg_memory) + num_bin_pattern; i++){
-                //cout << i << "\t" << r[i] << endl;
+                cout << i << "\t" << r[i] << "\t" << exp_amp[vec_to_encode[i - pow(2,2*len_reg_memory)]] << endl;
+//                REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+
             }
-  //cout << "------------" << endl;
+  cout << "------------" << endl;
             for(int i = pow(2,2*len_reg_memory) + num_bin_pattern; i < pow(2,num_qubits); i++){
-                //cout << i << "\t" << r[i] << endl;
+//                cout << i << "\t" << r[i] << endl;
                 REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
                 REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
 
             }
         
 
-            sim.collapseToBasisZ(reg_ancilla[len_reg_ancilla-2], 1);
             val = sim.applyMeasurementToRegister(reg_memory);
 
 //            sim.PrintStates("After Measurement: ");  
