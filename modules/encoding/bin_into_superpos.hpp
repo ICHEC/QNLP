df1074cdaf1dd63ee101ba46d0c1b335468d7801
Myles Doyle <myles.doyle@ichec.ie>
2020-01-16 12:50:25 +0000

    Refactored encoding directory: Added comments, deleted comments.

---
+++ modules/encoding/bin_into_superpos.hpp
@@ -0,0 +1,202 @@
+/**
+ * @file bin_into_superpos.hpp
+ * @author Myles Doyle (myles.doyle@ichec.ie)
+ * @brief Functions for encoding binary numbers into a superposition of states
+ * @version 0.1
+ * @date 2019-29-06
+ */
+
+#ifndef QNLP_BINENCODE
+#define QNLP_BINENCODE
+
+#include <cassert>
+#include <utility>
+#include <memory>
+#include <cmath>
+#include<vector>
+#include<complex>
+#include<iostream>
+
+namespace QNLP{
+
+    /**
+     * @brief Checks if the bit'th bit of the integer byte is set
+     * 
+     * @param byte - integer representing a binary string
+     * @param bit - the index of the bit to be checked (beginning
+     * with the least significant bit)
+     */
+    #define IS_SET(byte,bit) (((byte) & (1UL << (bit))) >> (bit))
+
+    template <class SimulatorType>
+    class EncodeBinIntoSuperpos{
+        private:
+            using Mat2x2Type = decltype(std::declval<SimulatorType>().getGateX());
+
+            std::unique_ptr< std::vector<Mat2x2Type> > S;
+            std::size_t m, len_reg_auxiliary, len_bin_pattern;
+    
+        public:
+            EncodeBinIntoSuperpos(){};
+
+            EncodeBinIntoSuperpos(const std::size_t num_bin_patterns, const std::size_t len_bin_pattern_){
+                m = num_bin_patterns;
+                len_bin_pattern = len_bin_pattern_;
+
+                initialiseMats();
+            };
+
+            ~EncodeBinIntoSuperpos(){
+            };
+
+            /**
+             * @brief Define the PauliX and the unitary matrix S
+             * 
+             */
+            void initialiseMats(){
+
+                /* Preparation for binary encoding:
+                 *
+                 * Prepare three matrices S^1,S^2,...,S^m that are required for the implemented
+                 * algorithm to encode these binary strings into a superposition state.
+                 *
+                 * Note the matrix indexing of the S vector of S^p matrices will be backwards:
+                 *      S[0] -> S^p
+                 *      S[1] -> S_{p-1}, and so on.
+                 */
+                S.reset(new std::vector<Mat2x2Type> (m));
+                {
+                    int p = m;
+                    double diag, off_diag;
+
+                    for(std::size_t i = 0; i < m; i++){
+                        off_diag = 1.0/sqrt((double)(p));
+                        diag = off_diag * sqrt((double)(p-1));
+
+                        (*S)[i](0,0) = {diag, 0.0};
+                        (*S)[i](0,1) = {off_diag, 0.0};
+                        (*S)[i](1,0) = {-off_diag, 0.0};
+                        (*S)[i](1,1) = {diag, 0.0};
+
+                        p--;
+                    }
+                }
+            }
+
+            /**
+             * @brief Encodes each element of inputted vector as a binary string in a superpostiion of states. Requires each binary input to be unique
+             * 
+             * @param qReg Qubit register
+             * @param reg_memory A vector containing the indices of the qubits of the memory register. 
+             * @param reg_auxiliary A vector containing the indices of the qubits of the auxiliary register. 
+             * @param Vector of non-negative integers which represent the inputted binary patters that are to be encoded.
+             */
+            void encodeBinInToSuperpos_unique(SimulatorType& qSim, 
+                    const std::vector<std::size_t>& reg_memory,
+                    const std::vector<std::size_t>& reg_auxiliary, 
+                    const std::vector<std::size_t>& bin_patterns){
+                std::size_t len_reg_auxiliary = reg_auxiliary.size();
+
+                // Require length of auxiliary register to have n+2 qubits
+                assert(reg_memory.size() + 1 < len_reg_auxiliary);
+
+                // Prepare state in |0...>|0...0>|10> of lengths n,n,2
+                #ifdef GATE_LOGGING
+                qSim.getGateWriter().segmentMarkerOut("Prepare state in |0...>|0...0>|10> of lengths n,n,2");
+                #endif
+                qSim.applyGateX(reg_auxiliary[len_reg_auxiliary-1]);
+                std::vector<std::size_t> sub_reg(reg_memory.begin(), reg_memory.begin () + len_bin_pattern);
+
+                // Iteratively encode each binary pattern.
+                for(std::size_t i = 0; i < m; i++){
+
+                    // Psi0
+                    // Encode inputted binary pattern.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_0 \\rangle");
+                    #endif
+                    for(std::size_t j = 0; j < len_bin_pattern; j++){
+                        if(IS_SET(bin_patterns[i],j)){
+                            qSim.applyGateX(reg_auxiliary[j]);
+                        }
+                    }
+
+                    // Psi1
+                    // Copy pattern to auxiliary register of newly created state (inow becoming the `active` state).
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_1 \\rangle");
+                    #endif
+                    for(std::size_t j = 0; j < len_bin_pattern; j++){
+                        qSim.applyGateCCX(reg_auxiliary[j], reg_auxiliary[len_reg_auxiliary-1], reg_memory[j]);
+                    }
+
+                    // Psi2
+                    // Set memory register to state |11..1> if it is the active state.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_2 \\rangle");
+                    #endif
+                    for(std::size_t j = 0; j < len_bin_pattern; j++){
+                        qSim.applyGateCX(reg_auxiliary[j], reg_memory[j]);
+                        qSim.applyGateX(reg_memory[j]);
+                    }
+
+                    // Psi3
+                    // Apply NCU to flip qubit in second auxiliary register (index `len_reg_auxiliary-2`).
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_3 \\rangle");
+                    #endif
+                    std::vector<std::size_t> tmp_aux(reg_auxiliary.begin(), reg_auxiliary.begin() + sub_reg.size()-2 );
+                    qSim.applyGateNCU(qSim.getGateX(), sub_reg, tmp_aux, reg_auxiliary[len_reg_auxiliary-2], "X");
+
+                    // Psi4
+                    // Apply S^i
+                    // This flips the second control bit of the new term in the position so
+                    // that we get old|11> + new|10> thus breaking it off into larger and smaller chunks.
+                    // The new state is now defined as the next 'newly created state'.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_4 \\rangle");
+                    #endif
+                    qSim.applyGateCU((*S)[i], reg_auxiliary[len_reg_auxiliary-2], reg_auxiliary[len_reg_auxiliary-1]);
+
+                    // Psi5
+                    // Uncompute NCU
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_5 \\rangle");
+                    #endif
+                    qSim.applyGateNCU(qSim.getGateX(), sub_reg, tmp_aux, reg_auxiliary[len_reg_auxiliary-2], "X");                    
+
+                    // Psi6 
+                    // Uncompute setting of memory register to all 1's of active state.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_6 \\rangle");
+                    #endif
+                    for(int j = len_bin_pattern-1; j > -1; j--){
+                        qSim.applyGateX(reg_memory[j]);
+                        qSim.applyGateCX(reg_auxiliary[j], reg_memory[j]);
+                    }
+
+                    // Psi7
+                    // Uncompute encoding.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("| \\Psi_7 \\rangle");
+                    #endif
+                    for(int j = len_bin_pattern-1; j > -1; j--){
+                       qSim.applyGateCCX(reg_auxiliary[j], reg_auxiliary[len_reg_auxiliary-1], reg_memory[j]);
+                    }
+
+                    // Reset the register of the new term to the state |0...0>|0...0>|10>.
+                    #ifdef GATE_LOGGING
+                    qSim.getGateWriter().segmentMarkerOut("Reset p to | 00\\ldots 0 \\rangle");
+                    #endif
+                    for(std::size_t j = 0; j < len_bin_pattern; j++){
+                        // Check current pattern against next pattern
+                        if(IS_SET(bin_patterns[i],j)){
+                            qSim.applyGateX(reg_auxiliary[j]);
+                        }
+                    }
+                }
+            }
+    };
+
+};
+#endif
---
+++ modules/encoding/test_bin_into_superpos.cpp
@@ -0,0 +1,132 @@
+//#define CATCH_CONFIG_RUNNER
+
+#include "bin_into_superpos.hpp"
+#include "Simulator.hpp"
+#include "IntelSimulator.cpp"
+#include "catch2/catch.hpp"
+
+#include <bitset>
+
+using namespace QNLP;
+using namespace Catch::Matchers;
+
+typedef ComplexDP Type;
+
+template class QNLP::EncodeBinIntoSuperpos<IntelSimulator>;
+
+TEST_CASE("Test encoding of binary (integers) to superposition","[encode]"){
+    SECTION("Testing qubit encoding"){
+
+        std::size_t num_qubits = 10;
+
+        std::size_t len_reg_memory = (num_qubits - 2) / 2;
+        std::size_t len_reg_ancilla = len_reg_memory + 2;
+        std::size_t num_bin_pattern = pow(2,len_reg_memory) - 2;
+
+        IntelSimulator sim(num_qubits);
+        sim.initRegister();
+
+        REQUIRE(sim.getNumQubits() == num_qubits);
+
+        // Set up registers to store indices
+        std::vector<std::size_t> reg_memory(len_reg_memory);
+        for(std::size_t i = 0; i < len_reg_memory; i++){
+            reg_memory[i] = i;
+        }
+        std::vector<std::size_t> reg_ancilla(len_reg_ancilla);
+        for(std::size_t i = 0; i < len_reg_ancilla; i++){
+            reg_ancilla[i] = i + len_reg_memory;
+        }
+
+        // Init data to encode
+        std::vector<std::size_t> vec_to_encode(num_bin_pattern);
+        for(std::size_t i = 2; i < num_bin_pattern; i++){
+            vec_to_encode[i-2] = i;
+        }
+
+        std::size_t val;
+        // Encode
+        EncodeBinIntoSuperpos<decltype(sim)> encoder(num_bin_pattern, len_reg_memory);
+        encoder.encodeBinInToSuperpos_unique(sim, reg_memory, reg_ancilla, vec_to_encode);
+
+        // Measure
+        val = sim.applyMeasurementToRegister(reg_memory);
+
+        CHECK_THAT(vec_to_encode, VectorContains(val));
+    }
+}
+
+TEST_CASE("Test encoding of different register sizes and checking states' amplitudes","[encode_amp]"){
+    const std::size_t max_qubits = 5;
+    double mach_eps = 7./3. - 4./3. -1.;
+
+    std::size_t num_qubits;
+    std::size_t len_reg_ancilla;
+    std::size_t num_bin_pattern;
+    std::size_t val;
+
+    std::size_t test_pattern = 2;
+
+    double expected_val;
+
+    for(std::size_t len_reg_memory = 2; len_reg_memory < max_qubits; len_reg_memory++){
+        DYNAMIC_SECTION("Testing " << len_reg_memory << " memory qubits"){
+
+            // Experiment set-up
+            num_qubits = 2*len_reg_memory + 2;
+            len_reg_ancilla = len_reg_memory + 2;
+            num_bin_pattern = pow(2,len_reg_memory);
+
+            // Expected real part of amplitude for equally encoded states
+            expected_val = sqrt(1.0 / (double) num_bin_pattern);
+
+            IntelSimulator sim(num_qubits);
+            sim.initRegister();
+            auto &r = sim.getQubitRegister();
+
+            // Set up registers to store indices
+            std::vector<std::size_t> reg_memory(len_reg_memory);
+            for(std::size_t i = 0; i < len_reg_memory; i++){
+                reg_memory[i] = i;
+            }
+            std::vector<std::size_t> reg_ancilla(len_reg_ancilla);
+            for(std::size_t i = 0; i < len_reg_ancilla; i++){
+                reg_ancilla[i] = i + len_reg_memory;
+            }
+
+            // Init data to encode
+            std::vector<std::size_t> vec_to_encode(num_bin_pattern);
+            for(std::size_t i = 0; i < num_bin_pattern; i++){
+                vec_to_encode[i] = i;
+            }
+
+            // Encode
+            sim.encodeBinToSuperpos_unique(reg_memory, reg_ancilla, vec_to_encode, len_reg_memory);
+
+            for(int i = 0; i < num_bin_pattern; i++){
+                REQUIRE(r[i].real() + 10*mach_eps == Approx(expected_val).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+
+            }
+            for(int i = num_bin_pattern; i < pow(2,2*len_reg_memory); i++){
+                REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+            }
+            for(int i = pow(2,2*len_reg_memory); i < pow(2,2*len_reg_memory) + num_bin_pattern; i++){
+                REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+            }
+            for(int i = pow(2,2*len_reg_memory) + num_bin_pattern; i < pow(2,num_qubits); i++){
+                REQUIRE(r[i].real() + 10*mach_eps == Approx(0.).margin(1e-12));
+                REQUIRE(r[i].imag() + 10*mach_eps == Approx(0.).margin(1e-12) );
+            }
+        
+
+            sim.collapseToBasisZ(reg_ancilla[len_reg_ancilla-2], 1);
+            val = sim.applyMeasurementToRegister(reg_memory);
+
+            CHECK_THAT(vec_to_encode, VectorContains(val));
+
+        }
+    }
+}
