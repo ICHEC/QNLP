e9c9441138e89f29687a7ff3e24b076efd7fadc2
Myles Doyle <myles.doyle@ichec.ie>
2020-01-17 12:24:40 +0000

    Removed unnecessary and deprecated tests which were commented out.

--- modules/simulator/test_simulator.cpp
+++ modules/simulator/test_simulator.cpp
@@ -161,33 +161,6 @@ TEST_CASE("Measurement of qubits"){
 
 
 TEST_CASE("Encoding even distribution: Unique Binary Patterns"){
-    
-    /*  // Not included as CHECK_THROWS does not work with assert()
-    SECTION("Check pattern passed is unique assert works"){
-        std::size_t num_qubits_mem = 3;
-        std::vector<std::size_t> reg_mem(num_qubits_mem);
-        for(std::size_t i = 0; i < num_qubits_mem; i++){
-            reg_mem[i] = i;
-        }
-        std::vector<std::size_t> reg_ancilla(num_qubits_mem+2);
-        for(std::size_t i = 0; i < num_qubits_mem + 2; i++){
-            reg_ancilla[i] = i + num_qubits_mem;
-        }
-
-        std::size_t num_bin_patterns = 4; 
-
-        std::vector<std::size_t> bin_patterns(num_bin_patterns);
-        bin_patterns[0] = 1;
-        bin_patterns[1] = 2;
-        bin_patterns[2] = 1;
-        bin_patterns[3] = 3;
-
-        SimulatorGeneral<IntelSimulator> *sim = new IntelSimulator(2*num_qubits_mem + 2);
-
-        CHECK_THROWS(sim->encodeBinToSuperpos_unique(reg_mem, reg_ancilla, bin_patterns,num_qubits_mem));
-    }
-    */
-
     SECTION("Check encoding works correctly without throws and with expected distribution"){
         std::size_t max_num_qubits_mem = 6;
         std::size_t num_exp = 500;
@@ -256,7 +229,6 @@ TEST_CASE("Hamming distance check"){
 
         std::size_t result;
 
-
         // Repeat test for varying numbers of qubits
         for( std::size_t num_qubits_mem = 3; num_qubits_mem < max_num_qubits_mem; num_qubits_mem++){
             DYNAMIC_SECTION("Encoding " << num_qubits_mem << " qubits"){
@@ -295,8 +267,6 @@ TEST_CASE("Hamming distance check"){
                     count_bin_pattern[result]++;
                 }
 
-
-
                 // Check resulting distribution of results lies within an acceptable limit.
                 // For smaller sample sizes ~500, 30% epsilon value is qualitatively said to be an acceptable limit.
                 Approx target = Approx(num_exp / num_bin_patterns).epsilon(0.30);
@@ -311,118 +281,3 @@ TEST_CASE("Hamming distance check"){
 }
 
 
-
-
-/*  // encodBinTo Superpos is deprecated due to issue with algorithm. Thus only the encode..._unique is valid.
-TEST_CASE("Encoding even distribution: Binary"){
-
-    //  // Not included as CHECK_THROWS does not work with assert()
-    SECTION("Check pattern passed is does not contain a zero assert works"){
-        std::vector<std::size_t> reg_mem(num_qubits_mem);
-        for(std::size_t i = 0; i < num_qubits_mem; i++){
-            reg_mem[i] = i;
-        }
-        std::vector<std::size_t> reg_ancilla(num_qubits_mem+2);
-        for(std::size_t i = 0; i < num_qubits_mem + 2; i++){
-            reg_ancilla[i] = i + num_qubits_mem;
-        }
-
-        std::size_t num_bin_patterns = 4; 
-
-        std::vector<std::size_t> bin_patterns(num_bin_patterns);
-        bin_patterns[0] = 1;
-        bin_patterns[1] = 0;
-        bin_patterns[2] = 1;//(std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2);
-        bin_patterns[3] = 3;//((std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2)) << (num_qubits_mem / 2);
-
-        SimulatorGeneral<IntelSimulator> *sim = new IntelSimulator(2*num_qubits_mem + 2);
-
-        CHECK_THROWS(sim->encodeBinToSuperpos(reg_mem, reg_ancilla, bin_patterns,num_qubits_mem));
-    }
-    
-
-
-
-    SECTION("Check encoding works correctly without throws and with expected distribution"){
-        std::size_t max_num_qubits_mem = 6;
-        std::size_t num_exp = 500;
-        std::size_t result;
-
-        // Repeat test for varying numbers of qubits
-        for( std::size_t num_qubits_mem = 3; num_qubits_mem < max_num_qubits_mem; num_qubits_mem++){
-            DYNAMIC_SECTION("Encoding " << num_qubits_mem << " qubits"){
-
-                std::vector<std::size_t> reg_mem(num_qubits_mem);
-                for(std::size_t i = 0; i < num_qubits_mem; i++){
-                    reg_mem[i] = i;
-                }
-                std::vector<std::size_t> reg_ancilla(num_qubits_mem+2);
-                for(std::size_t i = 0; i < num_qubits_mem + 2; i++){
-                    reg_ancilla[i] = i + num_qubits_mem;
-                }
-
-                std::size_t num_bin_patterns = 3; 
-
-                std::vector<std::size_t> bin_patterns(num_bin_patterns);
-                //bin_patterns[0] = pow(2,num_qubits_mem)-4;
-                //bin_patterns[1] = 1;
-                //bin_patterns[2] = (std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2);
-                //bin_patterns[3] = ((std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2)) << (num_qubits_mem / 2);
-                bin_patterns[0] = 2;
-                bin_patterns[1] = 1;
-                bin_patterns[2] = 1;//(std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2);
-                //bin_patterns[3] = 4;//((std::size_t)pow(2,num_qubits_mem)-1 >> (num_qubits_mem / 2)) << (num_qubits_mem / 2);
-
-
-
-                std::map<std::size_t, std::size_t> count_bin_pattern;
-                for(std::size_t i = 0; i < num_bin_patterns; i++){
-                    count_bin_pattern.insert(std::pair<std::size_t,std::size_t>(bin_patterns[i],0));
-                }
-
-
-
-                SimulatorGeneral<IntelSimulator> *sim = new IntelSimulator(2*num_qubits_mem + 2);
-
-                for(std::size_t exp = 0; exp < num_exp; exp++){
-                    
-                    sim->initRegister();
-                    sim->encodeBinToSuperpos(reg_mem, reg_ancilla, bin_patterns,num_qubits_mem);
-                    result = sim->applyMeasurementToRegister(reg_mem);
-
-                    // Check measured result is valid
-                    CHECK_THAT(bin_patterns, Catch::Matchers::VectorContains(result));
-
-                    // Update distributions of the results
-                    count_bin_pattern[result]++;
-                }
-
-                // Check resulting distribution of results lies within an acceptable limit.
-                // For smaller sample sizes ~500, 30% epsilon value is qualitatively said to be an acceptable limit.
-                Approx target = Approx(num_exp / num_bin_patterns).epsilon(0.30);
-                for(std::map<std::size_t,std::size_t>::iterator it = count_bin_pattern.begin(); it != count_bin_pattern.end(); it++){
-                    CHECK( target == (double) it->second);
-                }
-                
-            }
-
-        }
-
-    }
-}
-*/
-
-/**
- * @brief User defined main required for this instance, as openqu::mpi::Environment destructor calls MPI_Finalize.
- * As we require MPI until the end of the test session, it must be created before the tests, and destroyed after
- * all have finished.
- */
-/*
-int main( int argc, char* argv[] ) {
-    openqu::mpi::Environment env(argc, argv);
-
-    int result = Catch::Session().run( argc, argv );
-
-    return result;
-}
-*/
